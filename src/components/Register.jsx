be3413aef2662317062a404513f1a67fea808148
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-04 17:07:01 -0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom'
 
 function Register() {
 const [user, setUser] = useState({
   email: "",
   password: "",
 })
+const [error, setError] = useState()
+const {signup} = useAuth()
+const navigate = useNavigate()
 
 const handleChange = ({target: {name, value}}) => {
   setUser({...user, [name]: value})
 }
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault()
-  console.log(user)
+  try {
+    await signup(user.email, user.password)
+    navigate("/")
+  } catch (error) {
+    setError(error.message)
+  }
 }
-
+console.log(error)
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="email">Email</label>
