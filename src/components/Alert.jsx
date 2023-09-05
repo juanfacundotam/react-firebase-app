5c9d2db1182c41fe718dbfebf7bdeb63f2c6402d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-05 17:56:23 -0300

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Alert({message}) {
   return (
-    <div><span>{message}</span></div>
+    <div className="absolute rounded-md border-2 border-[#4242428f] p-2 -top-20 text-red-500"><span>{message}</span></div>
   )
 }
