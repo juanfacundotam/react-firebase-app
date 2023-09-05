111cb989a2eae9d6416adf0f5fbd71d85d61f6f4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-05 00:07:02 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-// import { useAuth } from '../context/authContext'
-
+import React from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  // const authContext = useAuth()
-  // console.log(authContext)
+  const { user, logout, loading } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.log(error)
+    }
+    // navigate("/login");  Ya no haria falta esto porque lo hace el componente ProtectedRoute
+  };
+
+if(loading) return (<h1>Loading</h1>)
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Wellcome {user.displayName || user.email}</h1>
+
+      <button onClick={handleLogout}>logout</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
