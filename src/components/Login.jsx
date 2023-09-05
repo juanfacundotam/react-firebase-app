ba7219662fc5d7c38b212ed621db26f68df6a4ee
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-05 00:06:59 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,81 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
-const [user, setUser] = useState({email: '', password:''})
+function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState();
+  const { login, loginWithGoogle } = useAuth();
+  const navigate = useNavigate();
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await login(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === "auth/invalid-email") {
+        setError("Cuenta de correo invalida");
+      }
+      if (error.code === "auth/wrong-password") {
+        setError("Password incorrecto");
+      }
+      if (error.code === "auth/weak-password") {
+        setError("Contraseña: Más de 6 caracteres");
+      }
+      if (error.code === "auth/user-not-found") {
+        setError("Cuenta de correo inexistente");
+      }
+      if (error.code === "auth/too-many-requests") {
+        setError("Has superado la cantidad de intentos");
+      }
+    }
+  };
+
+  const handleGoogleSignin = async () => {
+    try {
+      await loginWithGoogle();
+      navigate("/")
+    } catch (error) {
+      setError(error.message)
+    }
+  };
 
   return (
     <div>
-      <form action="">
-        <input type="email" name='email' id='email' />
-        <input type="password" name='password' id='password' />
+      <form onSubmit={handleSubmit}>
+        {error && <Alert message={error}/>}
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="example@email.com"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button>Login</button>
       </form>
+
+      <button onClick={handleGoogleSignin}>Login with Google</button>
     </div>
-  )
+  );
 }
+
+export default Login;
