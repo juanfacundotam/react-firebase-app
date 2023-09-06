3eb7b41df3e99f150aebc4def1c580af69ddb0bd
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-06 09:23:13 -0300

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from "../assets/TAMCO.webp";
+export default function Logo() {
+  return (
+    <div className="w-96 h-20 my-5 overflow-hidden flex justify-start items-center">
+    <img src={logo} alt="imagen del logo" classname="w-96" />
+  </div>
+  )
+}
