aefbe09b31b3b68670d0835d0e803a58f9bf8d1f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-06 10:06:52 -0300

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/TAMCO.webp";
 export default function Logo() {
   return (
     <div className="w-96 h-20 my-5 overflow-hidden flex justify-start items-center">
-    <img src={logo} alt="imagen del logo" classname="w-96" />
+    <img src={logo} alt="imagen del logo" className="w-96" />
   </div>
   )
 }
