02e87a5c8650db3de06d2ae0960d2169ac671409
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-06 10:06:50 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -110,7 +110,7 @@ function Login() {
             />
           </div>
           <div className="flex justify-between items-center mt-3 w-full">
-            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-800 focus:outline-none dark:focus:ring-blue-800">
               Login
             </button>
             <Link
