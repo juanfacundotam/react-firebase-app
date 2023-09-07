63b7d50d60bd6d14c3ad86df40e30742696439f7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-07 17:58:00 -0300

    Update ListTask.jsx

--- src/components/Tareas/ListTask.jsx
+++ src/components/Tareas/ListTask.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import { useAuth } from "../../context/authContext";
+export default function ({ tareas, setTareas }) {
+  const { user, logout, deleteTask } = useAuth();
 
-export default function ({ tareas }) {
+  const deleteTaskHandler = async (taskId) => {
+    const updatedTasks = tareas.filter((tarea)=> tarea.id !== taskId)
+    setTareas(await deleteTask(updatedTasks));
+  };
   return (
-    <div>
-      {tareas.length && tareas.map((tarea, index) => {
-        return (
-          <div key={index}>
-            <div  className="flex items-center justify-center">
-              <p>{tarea.description}</p>
-              <button
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >Ver Archivo</button>
-              <button
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >Eliminar Archivo</button>
-            </div>
-            <div className="border-b-2 w-96"></div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {tareas && !!tareas.length && (
+        <div className="w-fit flex flex-col justify-center items-center border-2 mt-5">
+          {tareas.map((tarea, index) => {
+            return (
+              <div key={index}>
+                <div className="flex items-center justify-center">
+                  <p>{tarea.description}</p>
+                  <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Ver Archivo
+                  </button>
+                  <button
+                    onClick={() => deleteTaskHandler(tarea.id)}
+                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Eliminar Archivo
+                  </button>
+                </div>
+                <div className="border-b-2 w-96"></div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
