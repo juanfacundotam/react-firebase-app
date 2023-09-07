85804683051dc72a24531c0b4ee835f7c0d0082e
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-07 17:57:56 -0300

    Update CardEmployees.jsx

--- src/components/CardEmployees.jsx
+++ src/components/CardEmployees.jsx
@@ -4,14 +4,14 @@ export default function CardEmployees({name, image, rol}) {
   return (
     <div className="w-full md:w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <div className="flex justify-end px-4 pt-4">
-        <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+        {/* <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
             <span className="sr-only">Open dropdown</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
                 <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
             </svg>
-        </button>
+        </button> */}
         {/* DropMenu */}
-        <div id="dropdown" className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+        {/* <div id="dropdown" className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
             <ul className="py-2" aria-labelledby="dropdownButton">
             <li>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
@@ -23,10 +23,10 @@ export default function CardEmployees({name, image, rol}) {
                 <a href="#" className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
             </li>
             </ul>
-        </div>
+        </div> */}
     </div>
-    <div className="flex flex-col items-center pb-10">
-        <img className="w-20 h-20 mb-3 rounded-full shadow-lg" src={image} alt="Bonnie image"/>
+    <div className="flex flex-col justify-center items-center pb-10">
+        <img className="w-20 h-20 mb-3 rounded-full shadow-lg flex justify-center items-center" src={image} alt="Imagen"/>
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">{rol}</span>
         <div className="flex mt-4 space-x-3 md:mt-6">
