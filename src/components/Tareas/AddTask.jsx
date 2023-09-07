66a39ea437b2cf782826cedb04b01a417ef739cb
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-07 17:57:58 -0300

    Update AddTask.jsx

--- src/components/Tareas/AddTask.jsx
+++ src/components/Tareas/AddTask.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function AddTask() {
+  const { user } = useAuth();
+  const newTaskHandler = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>AddTask</div>
-  )
+    <div className="w-fit flex flex-col justify-center items-center border-2 mt-5 text-black">
+      <form onSubmit={newTaskHandler} className="flex items-center justify-center">
+        <input type="text" placeholder="Describe tu tarea"/>
+        <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Ver Archivo
+        </button>
+        <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Eliminar Archivo
+        </button>
+      </form>
+
+    </div>
+  );
 }
