2eec6c76b161eaff8d5fc89c5b162bb4ddcec7c7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-07 19:30:54 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const firestore = getFirestore(app)
-const analytics = getAnalytics(app);
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp)
+export const firestore = getFirestore(firebaseApp)
+export const storage = getStorage(firebaseApp)
+const analytics = getAnalytics(firebaseApp);
