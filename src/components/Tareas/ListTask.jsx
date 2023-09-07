09be5ef0aea8524cf5cc40c4f97da7cf41bd8283
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-07 19:30:59 -0300

    Update ListTask.jsx

--- src/components/Tareas/ListTask.jsx
+++ src/components/Tareas/ListTask.jsx
@@ -16,9 +16,9 @@ export default function ({ tareas, setTareas }) {
               <div key={index}>
                 <div className="flex items-center justify-center">
                   <p>{tarea.description}</p>
-                  <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <a href={tarea.url} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Ver Archivo
-                  </button>
+                  </a>
                   <button
                     onClick={() => deleteTaskHandler(tarea.id)}
                     className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
