d9a7ac75e66ba268997268e8fbc69dce80e30992
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-10 11:48:25 -0300

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useAuth } from '../context/authContext'
 import { Navigate } from 'react-router-dom'
+import Spinner from './spinner'
 
 function ProtectedRoute({children}) {
   const {user, loading} = useAuth()
 
-  if(loading) return <h1>loading</h1>
+  if(loading) return (
+  <div className=" flex h-screen justify-center items-center">
+    <Spinner/>
+  </div>)
   
   if(!user) return <Navigate to="/login"/>
 
