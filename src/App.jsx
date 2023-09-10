1c678c7bb57dcde47eb44ebffc7a77aaaba38fde
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-10 12:03:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
             element={
               <ProtectedRoute>
                 <Home />
-                <Chat/>
               </ProtectedRoute>
             }
           />
