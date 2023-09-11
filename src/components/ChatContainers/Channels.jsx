4525a3c6d1743b0abc97b46aa1eb5b345e0dc2e4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 17:12:21 -0300

    Update Channels.jsx

--- src/components/ChatContainers/Channels.jsx
+++ src/components/ChatContainers/Channels.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import ChannelsContainer from './ChannelsComponents/ChannelsContainer'
-import ContactsContainer from './ChannelsComponents/ContactsContainer'
+import React from "react";
+import ChannelsContainer from "./ChannelsComponents/ChannelsContainer";
+import ContactsContainer from "./ChannelsComponents/ContactsContainer";
+import PublicChannels from "./ChannelsComponents/PublicChannels";
+import PrivateChannels from "./ChannelsComponents/PrivateChannels";
 
 export default function Channels() {
   return (
     <>
-    <ChannelsContainer/>
-    <ContactsContainer/>
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+        <PublicChannels />
+      </div>
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+        <PrivateChannels />
+      </div>
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+        <ContactsContainer />
+      </div>
     </>
-  )
+  );
 }
