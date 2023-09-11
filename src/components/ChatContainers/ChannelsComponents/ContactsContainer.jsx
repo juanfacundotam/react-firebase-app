bb5bdccddc397ebd320c856c82e5a84545cbc742
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:20:19 -0300

    Create ContactsContainer.jsx

---
+++ src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function ContactsContainer() {
+  return (
+    <div className="border-2 flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 ">
+      <h1>v Contactos</h1>
+      <div className="ml-5 mt-2 overflow-scroll w-full">
+        <p>Pedro Juan Gomez</p>
+        <p>Ricardo Iorio</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+        <p>Tomas Achaval</p>
+      </div>
+    </div>
+  );
+}
