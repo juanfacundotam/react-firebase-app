9332e79430512642054799c0ff16d356dd456ab7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:49:22 -0300

    Update ChannelsContainer.jsx

--- src/components/ChatContainers/ChannelsComponents/ChannelsContainer.jsx
+++ src/components/ChatContainers/ChannelsComponents/ChannelsContainer.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function ChannelsContainer() {
   return (
-    <div className='border-2 flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 '>
+    <div className="border-2 flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 ">
       <h1>v Canales</h1>
-      <div className='ml-5 mt-2 overflow-scroll w-full'>
-      <p># General</p>
-      <p># Grupo A</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
-      <p># Grupo B</p>
+      <div className="border-2  mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-96">
+      <p className="ml-5"># General</p>
+      <p className="ml-5"># Grupo A</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
+      <p className="ml-5"># Grupo B</p>
       </div>
     </div>
   )
