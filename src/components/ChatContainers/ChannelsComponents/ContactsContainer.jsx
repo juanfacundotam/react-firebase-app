c8b8edcd01edf7e218cd08693771491be1f5b6e2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:49:20 -0300

    Update ContactsContainer.jsx

--- src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
+++ src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
@@ -4,19 +4,20 @@ export default function ContactsContainer() {
   return (
     <div className="border-2 flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 ">
       <h1>v Contactos</h1>
-      <div className="ml-5 mt-2 overflow-scroll w-full">
-        <p>Pedro Juan Gomez</p>
-        <p>Ricardo Iorio</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
-        <p>Tomas Achaval</p>
+      <div className="border-2  mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-96">
+        <p className="ml-5">Pedro Juan Gomez</p>
+        <p className="ml-5">Ricardo Iorio</p>
+        <p className="ml-5">Tomas Achaval</p>
+        <p className="ml-5">Tomas Achaval</p>
+        <p className="ml-5">Tomas Achaval</p>
+        <p className="ml-5">Tomas Achaval</p>
+        <p className="ml-5">Tomas Achaval</p>
+        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
+        <p className="ml-5">Tomassssssssssssssssssssssssss Achaval</p>
+        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
+        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
+        <p className="ml-5">Tomas Achaval</p>
+
       </div>
     </div>
   );
