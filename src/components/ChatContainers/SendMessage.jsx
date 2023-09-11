b614339d81eef6ad3512b653eb1463aef9482884
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 17:12:33 -0300

    Update SendMessage.jsx

--- src/components/ChatContainers/SendMessage.jsx
+++ src/components/ChatContainers/SendMessage.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import GifBoxIcon from '@mui/icons-material/GifBox';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import GifBoxIcon from "@mui/icons-material/GifBox";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 export default function SendMessage() {
   return (
-    <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[80%] rounded-lg text-white gap-3 ">
-      <AddCircleIcon sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1}}/>
+    <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[75%] max-h-[50px] rounded-lg text-white gap-3 ">
+      <AddCircleIcon sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }} />
       <input
-        className=" bg-[#40414F] rounded py-2  focus:outline-none w-full"
+        className=" bg-[#40414F] rounded py-2  focus:outline-none w-full h-[100%]"
         type="text"
         placeholder="Escribe mensaje a #general"
       />
- <GifBoxIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1}}/>
- <EmojiEmotionsIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1}}/>
-
+      <GifBoxIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1 }} />
+      <EmojiEmotionsIcon
+        sx={{ fontSize: 25, cursor: "pointer", marginRight: 1 }}
+      />
     </div>
   );
 }
