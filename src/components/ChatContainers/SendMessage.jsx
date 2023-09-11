d725b22d8acf31dc9b741f83931ec949bedfd6be
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:57:46 -0300

    Update SendMessage.jsx

--- src/components/ChatContainers/SendMessage.jsx
+++ src/components/ChatContainers/SendMessage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function SendMessage() {
   return (
-    <div className=' bg-[#313338] flex justify-center items-center w-[95%] h-[80%] rounded-lg text-white '>
-        <input className=' bg-[#40414F] rounded py-2 px-3 focus:outline-none w-full' type="text" placeholder='Escribe mensaje a #general'/>
+    <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[80%] rounded-lg text-white gap-3 ">
+      <h1 className="border-2 p-2 cursor-pointer">%</h1>
+      <input
+        className=" bg-[#40414F] rounded py-2  focus:outline-none w-full"
+        type="text"
+        placeholder="Escribe mensaje a #general"
+      />
+   <h1 className="border-2 p-2 cursor-pointer">%</h1>
+   <h1 className="border-2 p-2 cursor-pointer">%</h1>
+   <h1 className="border-2 p-2 cursor-pointer">%</h1>
+   <h1 className="border-2 p-2 cursor-pointer">%</h1>
+
     </div>
-  )
+  );
 }
-
-
