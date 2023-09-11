2282dd271db74c41313bfdb34d6c7dad15e8cc9d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 14:08:49 -0300

    Update UpBar.jsx

--- src/components/ChatContainers/UpBar.jsx
+++ src/components/ChatContainers/UpBar.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import SearchIcon from '@mui/icons-material/Search';
 export default function UpBar() {
   return (
-    <div className='flex justify-between items-center w-[98%] h-full'>
-        <div><h1>#General</h1></div>
-        <div className='flex justify-between items-center gap-3 '>
-            <h1>%</h1>
-            <h1>%</h1>
-            <h1>%</h1>
-            <h1>%</h1>
-            <div className=' flex justify-between items-center bg-[#40414F] rounded   ' >
-            <input type="text" className='bg-[#40414F] rounded py-1 pl-3 focus:outline-none ' />
-<h1 className=' px-2 cursor-pointer'>#</h1>
-            </div>
-            <h1>%</h1>
-            <h1>%</h1>
-            <h1>%</h1>
+    <div className="flex justify-between items-center w-[98%] h-full">
+      <div>
+        <h1>#General</h1>
+      </div>
+      <div className="flex justify-between items-center gap-1 ">
+        <AddCircleIcon
+          sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }}
+        />
+        <AddCircleIcon
+          sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }}
+        />
+
+
+        <div className=" flex justify-between items-center bg-[#40414F] rounded   ">
+          <input
+            type="text"
+            className="bg-[#40414F] rounded py-1 pl-3 focus:outline-none "
+          />
+                <SearchIcon sx={{ fontSize: 25, marginRight:1, cursor: "pointer", marginLeft: 1}}/>
         </div>
+
+
+        <AddCircleIcon
+          sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }}
+        />
+      </div>
     </div>
-  )
+  );
 }
