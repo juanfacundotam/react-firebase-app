eb63a16ef67485ccfff714d4625dd71da020cd81
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:20:17 -0300

    Create ChannelsContainer.jsx

---
+++ src/components/ChatContainers/ChannelsComponents/ChannelsContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function ChannelsContainer() {
+  return (
+    <div className='border-2 flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 '>
+      <h1>v Canales</h1>
+      <div className='ml-5 mt-2 overflow-scroll w-full'>
+      <p># General</p>
+      <p># Grupo A</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      <p># Grupo B</p>
+      </div>
+    </div>
+  )
+}
