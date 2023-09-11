dc86f6a0ff57a84cbe6064c63855b8f0efe63e8d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 14:08:53 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -6,18 +6,18 @@ import BodyMessage from "../components/ChatContainers/BodyMessage";
 import SendMessage from "../components/ChatContainers/SendMessage";
 export default function Chat() {
   return (
-    <div className="border-2 flex justify-center items-center h-screen relative">
-      <div className="border-2 bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
+    <div className="flex justify-center items-center h-screen relative">
+      <div className="bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
         <Channels />
       </div>
-      <div className="border-2 bg-[#343541] flex flex-col justify-center items-center h-full w-[80%]">
-        <div className="border-b-2 border-black flex flex-col justify-center items-center h-[10%] w-full">
+      <div className="bg-[#343541] flex flex-col justify-center items-center h-full w-[80%]">
+        <div className="border-b-[1px] border-[#646464] flex flex-col justify-center items-center h-[10%] w-full">
           <UpBar />
         </div>
-        <div className="border-2 bg-[#343541] flex flex-col justify-center items-center h-[80%] w-full">
+        <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-center items-center h-[80%] w-full mt-2">
           <BodyMessage />
         </div>
-        <div className="border-2 bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
+        <div className=" bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
           <SendMessage />
         </div>
       </div>
