4466ba9d9c32481592dd48bceda2565612165925
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 10:56:51 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Channels from "../components/ChatContainers/Channels";
+import UpBar from "../components/ChatContainers/UpBar";
+import BodyMessage from "../components/ChatContainers/BodyMessage";
+import SendMessage from "../components/ChatContainers/SendMessage";
 export default function Chat() {
   return (
-    <div className="flex justify-center items-center h-screen relative">
-      <div>
-      <Link to="/">Home</Link>
+    <div className="border-2 flex justify-center items-center h-screen relative">
+      <div className="border-2 flex justify-center items-center h-full w-[20%]">
+        <Channels />
       </div>
-      <div>
-        <div>
-
+      <div className="border-2 flex flex-col justify-center items-center h-full w-[80%]">
+        <div className="border-b-2 border-black flex flex-col justify-center items-center h-[10%] w-full">
+          <UpBar />
         </div>
-        <div>
-
+        <div className="border-2 flex flex-col justify-center items-center h-[80%] w-full">
+          <BodyMessage />
         </div>
-        <div>
-          
+        <div className="border-2 flex flex-col justify-center items-center h-[10%] w-full">
+          <SendMessage />
         </div>
-      <h1>Chat</h1>
       </div>
     </div>
   );
