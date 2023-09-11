e95e73c22e22c745636259ceed81b098c4270d68
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 11:49:24 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -7,14 +7,14 @@ import SendMessage from "../components/ChatContainers/SendMessage";
 export default function Chat() {
   return (
     <div className="border-2 flex justify-center items-center h-screen relative">
-      <div className="border-2 flex flex-col justify-start items-center h-full w-[20%]">
+      <div className="border-2 bg-[#181818] flex flex-col justify-start items-center h-full w-[20%]">
         <Channels />
       </div>
       <div className="border-2 flex flex-col justify-center items-center h-full w-[80%]">
         <div className="border-b-2 border-black flex flex-col justify-center items-center h-[10%] w-full">
           <UpBar />
         </div>
-        <div className="border-2 flex flex-col justify-center items-center h-[80%] w-full">
+        <div className="border-2  flex flex-col justify-center items-center h-[80%] w-full">
           <BodyMessage />
         </div>
         <div className="border-2 flex flex-col justify-center items-center h-[10%] w-full">
