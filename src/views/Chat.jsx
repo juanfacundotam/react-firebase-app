aac09cd25d2741849f5ea031cd0ce4b952c0c3ce
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 10:01:30 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Chat() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen relative">
+    <div className="flex justify-center items-center h-screen relative">
+      <div>
       <Link to="/">Home</Link>
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
       <h1>Chat</h1>
+      </div>
     </div>
   );
 }
