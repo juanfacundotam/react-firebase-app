1c0b698240acc809335259ae25ef593fc4556288
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 14:08:47 -0300

    Update SendMessage.jsx

--- src/components/ChatContainers/SendMessage.jsx
+++ src/components/ChatContainers/SendMessage.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 export default function SendMessage() {
   return (
     <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[80%] rounded-lg text-white gap-3 ">
-      <h1 className="border-2 p-2 cursor-pointer">%</h1>
+      <AddCircleIcon sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1}}/>
       <input
         className=" bg-[#40414F] rounded py-2  focus:outline-none w-full"
         type="text"
         placeholder="Escribe mensaje a #general"
       />
-   <h1 className="border-2 p-2 cursor-pointer">%</h1>
-   <h1 className="border-2 p-2 cursor-pointer">%</h1>
-   <h1 className="border-2 p-2 cursor-pointer">%</h1>
-   <h1 className="border-2 p-2 cursor-pointer">%</h1>
+ <GifBoxIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1}}/>
+ <EmojiEmotionsIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1}}/>
 
     </div>
   );
