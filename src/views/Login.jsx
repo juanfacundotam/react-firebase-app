e5fb2b8978071a2156479bae2516a36f0bba0bf9
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-11 14:49:18 -0300

    Update Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -132,7 +132,7 @@ function Login() {
         <button
           onClick={handleGoogleSignin}
           type="button"
-          className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+          className="w-[98%] text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex justify-center items-center dark:focus:ring-[#4285F4]/55  mb-2"
         >
           <svg
             className="w-4 h-4 mr-2"
