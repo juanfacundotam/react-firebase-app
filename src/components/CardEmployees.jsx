a815b4e7a6bb398abd46362d2b6d05f613b2f44c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 11:45:43 -0300

    updated

--- src/components/CardEmployees.jsx
+++ src/components/CardEmployees.jsx
@@ -5,48 +5,92 @@ import iconImage from "../assets/logoloro.png";
 import SettingsHome from "./SettingsHome";
 import Spinner from "./spinner";
 
-export default function CardEmployees({ name, image, rol, changeSomethingInSettings }) {
+export default function CardEmployees({
+  nickName,
+  estado,
+  image,
+  rol,
+  setLoadSpinner,
+  loadSpinner,
+  getImage,
+  getDatos,
+  setDatos,
+  datos
+}) {
   const [editing, setEditing] = useState(false);
   const settingsHandler = () => {
     setEditing(!editing);
   };
   console.log(editing);
   return (
-    <div className="relative w-full md:w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex justify-end px-4 pt-4">
-        <SettingsIcon
-          sx={{
-            fontSize: 20,
-            cursor: "pointer",
-            transition: "transform 1s",
-            "&:hover": {
-              transform: "rotate(90deg)",
-            },
-          }}
-          onClick={settingsHandler}
-        />
-      </div>
-      {editing ? <SettingsHome settingsHandler={settingsHandler} image={image} changeSomethingInSettings={changeSomethingInSettings}/> : null}
-      <div className="flex flex-col justify-center items-center pb-10">
-        {image ?
-        <img
-          className=" w-20 h-20 mb-3 rounded-full shadow-lg flex justify-center items-center border-[1px] border-gray-700"
-          src={image}
-          alt="Imagen"
-        /> : <h1 className="border-[1px]  border-gray-500 w-20 h-20 mb-3 rounded-full shadow-lg flex justify-center items-center text-[#fafafa59]">Foto</h1> }
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          {name}
-        </h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">{rol}</span>
-        <Link
-          to="chat"
-          className="mt-7 cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 gap-3"
-        >
-          <img src={iconImage} alt="" className="  w-8 h-8 p-1 rounded-full" />
-          {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
-          <h1 className="">Ir al Chat</h1>
-        </Link>
-      </div>
+    <div className="relative flex flex-col justify-center items-center w-[500px] h-[300px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      {loadSpinner ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className=" w-full flex justify-end items-center px-4 pt-4">
+            <SettingsIcon
+              sx={{
+                fontSize: 20,
+                cursor: "pointer",
+                transition: "transform 1s",
+                "&:hover": {
+                  transform: "rotate(90deg)",
+                },
+              }}
+              onClick={settingsHandler}
+            />
+          </div>
+          {editing ? (
+            <SettingsHome
+              settingsHandler={settingsHandler}
+              nickName={nickName}
+              estado={estado}
+              image={image}
+              setDatos={setDatos}
+              setLoadSpinner={setLoadSpinner}
+              loadSpinner={loadSpinner}
+              getImage={getImage}
+              getDatos={getDatos}
+              datos={datos}
+            />
+          ) : null}
+          <div className="flex flex-col justify-center items-center pb-10">
+            {image ? (
+              <img
+                className=" w-20 h-20 mb-3 rounded-full shadow-lg flex justify-center items-center border-[1px] border-gray-700"
+                src={image}
+                alt="Imagen"
+              />
+            ) : (
+              <h1 className="border-[1px]  border-gray-500 w-20 h-20 mb-3 rounded-full shadow-lg flex justify-center items-center text-[#fafafa59]">
+                Foto
+              </h1>
+            )}
+            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+              {nickName}
+            </h5>
+            <div className="flex justify-center items-center w-[300px] h-fit whitespace-normal">
+
+            <p className="text-sm text-gray-500 dark:text-gray-400 text-center">
+              {estado}
+            </p>
+            </div>
+            <Link
+              to="chat"
+              className="mt-7 cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 gap-3"
+            >
+              <img
+                src={iconImage}
+                alt=""
+                className="  w-7 h-7 p-1 rounded-full"
+              />
+              {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
+              <h1 className="">Ir al Chat</h1>
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/ChatContainers/BodyComponents/Message.jsx
+++ src/components/ChatContainers/BodyComponents/Message.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
+import React from "react";
 
-export default function Message() {
+export default function Message({ datos, image}) {
   return (
-    <div className='flex justify-center items-center w-full h-full'>
-        <div className='flex justify-start items-start w-fit h-full'>
-            <h1 className='m-2 bg-black rounded-full w-10 h-10'></h1>
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="flex justify-start items-start w-fit h-full">
+        <img src={image} className="m-2  rounded-full w-10 h-10" alt="" />
+      </div>
+      <div className="flex flex-col justify-center items-end w-full h-full">
+        <div className="flex  justify-start items-end w-full h-[5%] gap-3">
+          
+          <div className="flex gap-2">
+            <h1 className="text-xs text-gray-200 mt-auto">{datos.nickName}</h1>
+            <h3 className="text-[0.6rem] text-gray-400 mt-auto">
+              19/10/2023 19:30
+            </h3>
+          </div>
+          <h2 className="text-[0.6rem] text-gray-400 mt-auto">{`"${datos.estado}"`}</h2>
+        
+        
         </div>
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='flex  justify-start items-center w-full h-[5%] gap-2'>
-                <h1 className='text-xs text-gray-200 mt-auto'>Juan Facundo Tam</h1>
-                <h3 className='text-[0.6rem] text-gray-400 mt-auto'>19/10/2023 19:30</h3>
-            </div>
-            <div className='text-[0.8rem] flex flex-col justify-start items-start w-full h-full gap-y-1'>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid nam eius quia eaque qui nulla corporis molestiae perspiciatis dolore facilis, officia vitae, sunt odit ea, vel tenetur ut eligendi. Velit!</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero fugit vitae iure optio nobis provident nostrum eaque voluptate ipsum vero nam, ratione obcaecati voluptas minus voluptatibus, sunt, distinctio incidunt laboriosam! Numquam, error ipsa? Quaerat rerum quibusdam nobis assumenda aliquid voluptates.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officiis aperiam numquam harum quibusdam vel.</p>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, quam?</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas eum nisi cumque inventore modi possimus beatae id eaque totam iste doloribus aliquid, veniam quas quam vitae repellat facere ipsa odio repudiandae quis in labore asperiores!</p>
-            </div>
+        <div className="text-[0.8rem] flex flex-col justify-start items-start w-full h-full gap-y-1">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+            nam eius quia eaque qui nulla corporis molestiae perspiciatis dolore
+            facilis, officia vitae, sunt odit ea, vel tenetur ut eligendi.
+            Velit!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            fugit vitae iure optio nobis provident nostrum eaque voluptate ipsum
+            vero nam, ratione obcaecati voluptas minus voluptatibus, sunt,
+            distinctio incidunt laboriosam! Numquam, error ipsa? Quaerat rerum
+            quibusdam nobis assumenda aliquid voluptates.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            officiis aperiam numquam harum quibusdam vel.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perferendis, quam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+            eum nisi cumque inventore modi possimus beatae id eaque totam iste
+            doloribus aliquid, veniam quas quam vitae repellat facere ipsa odio
+            repudiandae quis in labore asperiores!
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
