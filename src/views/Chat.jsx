32c8c0537bec2028b460c36bbfdafa48761e43fe
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 11:45:26 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/authContext";
 import Channels from "../components/ChatContainers/Channels";
 import UpBar from "../components/ChatContainers/UpBar";
 import BodyMessage from "../components/ChatContainers/BodyMessage";
 import SendMessage from "../components/ChatContainers/SendMessage";
+import Spinner from "../components/spinner";
+
 export default function Chat() {
+  const [loadSpinner, setLoadSpinner] = useState(true);
+  const [datos, setDatos] = useState({
+    nickName: "NickName",
+    estado: "Estado",
+  });
+  const [image, setImage] = useState("");
+
+  const { user, logout, loading, searchOrCreateDocument, searchOrCreateImage } =
+    useAuth();
+  useEffect( ( )  => {
+
+    getImage();
+    getDatos()
+    
+  }, []);
+
+  async function getDatos() {
+    const datosSearched = await searchOrCreateDocument(user.email);
+    console.log(datosSearched);
+    setDatos(datosSearched)
+    setLoadSpinner(false);
+  }
+  async function getImage() {
+    setImage(await searchOrCreateImage(user.email, user.photoURL));
+  }
   return (
     <div className="flex justify-center items-center h-screen relative">
-      <div className="bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
-        <Channels />
-      </div>
-      <div className="bg-[#343541] flex flex-col justify-center items-center h-full w-[80%]">
-        <div className="border-b-[1px] border-[#646464] flex flex-col justify-center items-center h-[8%] w-full">
-          <UpBar />
-        </div>
-        <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-center items-center h-[82%] w-full mt-2">
-          <BodyMessage />
-        </div>
-        <div className=" bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
-          <SendMessage />
-        </div>
-      </div>
+      {loadSpinner ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
+            <Channels />
+          </div>
+          <div className="bg-[#343541] flex flex-col justify-center items-center h-full w-[80%]">
+            <div className="border-b-[1px] border-[#646464] flex flex-col justify-center items-center h-[8%] w-full">
+              <UpBar />
+            </div>
+            <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-center items-center h-[82%] w-full mt-2">
+              <BodyMessage datos={datos} image={image}/>
+            </div>
+            <div className=" bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
+              <SendMessage />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
