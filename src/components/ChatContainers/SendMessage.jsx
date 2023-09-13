6f7c45bdf9bd98eb1a7598bd0435dfa0dbfe101f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 19:59:12 -0300

    Update SendMessage.jsx

--- src/components/ChatContainers/SendMessage.jsx
+++ src/components/ChatContainers/SendMessage.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import GifBoxIcon from "@mui/icons-material/GifBox";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-export default function SendMessage() {
+export default function SendMessage({setMessage, message, messageArray, setMessageArray}) {
+
+
+  const setMessageHandler = (e) => {
+setMessage(e.target.value)
+  }
+  console.log(message)
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      // Llama a tu función aquí cuando se presiona "Enter"
+      console.log("enviado: ", message)
+
+      setMessageArray([...messageArray, message])
+      setMessage("")
+ 
+    }
+  };
+
   return (
     <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[75%] max-h-[50px] rounded-lg text-white gap-3 ">
       <AddCircleIcon sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }} />
@@ -12,14 +29,15 @@ export default function SendMessage() {
         placeholder="Escribe mensaje a #general"
       /> */}
 
-<textarea
-  className="w-full min-h-[40px]  h-fit overflow-hidden bg-[#40414F] text-white p-2"
-  placeholder="Escribe un mensaje..."
-  // value={message}
-  // onChange={(e) => setMessage(e.target.value)}
-  // onKeyDown={handleKeyDown}
-
-></textarea>
+      <input
+      type="text"
+        name="message"
+        value={message}
+        className="w-full min-h-[40px] resize-y h-fit focus:outline-none overflow-hidden bg-[#40414F] text-white text-[13px] p-2"
+        placeholder="Escribe un mensaje..."
+        onChange={setMessageHandler}
+        onKeyDown={handleKeyPress}
+      />
 
       <GifBoxIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1 }} />
       <EmojiEmotionsIcon
