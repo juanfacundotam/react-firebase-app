56c72e27ace13f514bcfc92aba6719dc38727e6e
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 19:59:21 -0300

    Update ContactsContainer.jsx

--- src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
+++ src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
@@ -1,31 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 export default function ContactsContainer() {
+  const [openChannels, setOpenChannels] = useState(true)
+
   return (
-    <>
-      <div className="flex justify-center items-center">
-        {true ? (
+<>
+      <div className="flex justify-center items-center cursor-pointer hover:text-gray-300" 
+      onClick={() => {setOpenChannels(!openChannels)}}
+      >
+        {openChannels ? (
           <KeyboardArrowDownIcon sx={{ fontSize: 15, cursor: "pointer" }} />
         ) : (
           <KeyboardArrowUpIcon sx={{ fontSize: 15, cursor: "pointer" }} />
         )}
         <h1>Contactos</h1>
       </div>
-      <div className="mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-96">
-        <p className="ml-5">Pedro Juan Gomez</p>
-        <p className="ml-5">Ricardo Iorio</p>
-        <p className="ml-5">Tomas Achaval</p>
-        <p className="ml-5">Tomas Achaval</p>
-        <p className="ml-5">Tomas Achaval</p>
-        <p className="ml-5">Tomas Achaval</p>
-        <p className="ml-5">Tomas Achaval</p>
-        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
-        <p className="ml-5">Tomassssssssssssssssssssssssss Achaval</p>
-        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
-        <p className="ml-5">Tomasssssssssssssssssssssss Achaval</p>
-        <p className="ml-5">Tomas Achaval</p>
-      </div>
+
+      {openChannels && <div className="mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-fit">
+        <p className="ml-5"># General</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+        <p className="ml-5"># Grupo B</p>
+
+ 
+      </div>}
     </>
   );
 }
