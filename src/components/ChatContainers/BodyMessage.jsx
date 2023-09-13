beee6100869a13d39c1fae5978831cdf906485d5
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 11:45:34 -0300

    Update BodyMessage.jsx

--- src/components/ChatContainers/BodyMessage.jsx
+++ src/components/ChatContainers/BodyMessage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Message from './BodyComponents/Message'
+import Perfil from './BodyComponents/Perfil'
 
-export default function BodyMessage() {
+export default function BodyMessage({datos, image}) {
   return (
     <>
-    <Message/>
+    <Perfil datos={datos} image={image}/>
+    <Message datos={datos} image={image}/>
     </>
   )
 }
