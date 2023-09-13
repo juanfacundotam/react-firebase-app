7cbc5353c60178899fdee9944a921b5cdbd258e6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 12:07:15 -0300

    Update SendMessage.jsx

--- src/components/ChatContainers/SendMessage.jsx
+++ src/components/ChatContainers/SendMessage.jsx
@@ -6,11 +6,21 @@ export default function SendMessage() {
   return (
     <div className=" bg-[#40414F] flex justify-center items-center w-[95%] h-[75%] max-h-[50px] rounded-lg text-white gap-3 ">
       <AddCircleIcon sx={{ fontSize: 25, cursor: "pointer", marginLeft: 1 }} />
-      <input
-        className=" bg-[#40414F] rounded py-2  focus:outline-none w-full h-[100%]"
+      {/* <textarea
+        className=" bg-[#40414F] rounded S  focus:outline-none w-full h-[100%] overflow-hidden"
         type="text"
         placeholder="Escribe mensaje a #general"
-      />
+      /> */}
+
+<textarea
+  className="w-full min-h-[40px]  h-fit overflow-hidden bg-[#40414F] text-white p-2"
+  placeholder="Escribe un mensaje..."
+  // value={message}
+  // onChange={(e) => setMessage(e.target.value)}
+  // onKeyDown={handleKeyDown}
+
+></textarea>
+
       <GifBoxIcon sx={{ fontSize: 25, cursor: "pointer", marginRight: 1 }} />
       <EmojiEmotionsIcon
         sx={{ fontSize: 25, cursor: "pointer", marginRight: 1 }}
