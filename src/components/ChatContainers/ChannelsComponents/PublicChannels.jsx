fbeafa7bb7db01721f954230f7980bfc62a97f7b
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 19:59:15 -0300

    Update PublicChannels.jsx

--- src/components/ChatContainers/ChannelsComponents/PublicChannels.jsx
+++ src/components/ChatContainers/ChannelsComponents/PublicChannels.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 export default function PublicChannels() {
+  const [openChannels, setOpenChannels] = useState(true)
+
   return (
 <>
-      <div className="flex justify-center items-center">
-        {true ? (
+      <div className="flex justify-center items-center cursor-pointer hover:text-gray-300" 
+      onClick={() => {setOpenChannels(!openChannels)}}
+      >
+        {openChannels ? (
           <KeyboardArrowDownIcon sx={{ fontSize: 15, cursor: "pointer" }} />
         ) : (
           <KeyboardArrowUpIcon sx={{ fontSize: 15, cursor: "pointer" }} />
         )}
         <h1>Canales</h1>
       </div>
-      <div className="mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-96">
+
+      {openChannels && <div className="mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-fit">
         <p className="ml-5"># General</p>
         <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
+        <p className="ml-5"># Grupo A</p>
         <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-        <p className="ml-5"># Grupo B</p>
-      </div>
+
+ 
+      </div>}
     </>
   );
 }
