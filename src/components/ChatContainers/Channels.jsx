a9b88cf7ed9cbad7266f5795019b34232cf917f0
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 19:59:24 -0300

    Update Channels.jsx

--- src/components/ChatContainers/Channels.jsx
+++ src/components/ChatContainers/Channels.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ChannelsContainer from "./ChannelsComponents/ChannelsContainer";
 import ContactsContainer from "./ChannelsComponents/ContactsContainer";
 import PublicChannels from "./ChannelsComponents/PublicChannels";
 import PrivateChannels from "./ChannelsComponents/PrivateChannels";
 
 export default function Channels() {
+
+
   return (
     <>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
+    
+     <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%] mt-5 text-[0.8rem] ">
         <PublicChannels />
       </div>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
+      {/* <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
         <PrivateChannels />
-      </div>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
+      </div> */}
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%] mt-5 text-[0.8rem] ">
         <ContactsContainer />
       </div>
     </>
