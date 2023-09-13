74c4b230aede853f2e1f081faf6f0df1a249167c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-13 12:07:13 -0300

    Update Channels.jsx

--- src/components/ChatContainers/Channels.jsx
+++ src/components/ChatContainers/Channels.jsx
@@ -7,13 +7,13 @@ import PrivateChannels from "./ChannelsComponents/PrivateChannels";
 export default function Channels() {
   return (
     <>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
         <PublicChannels />
       </div>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
         <PrivateChannels />
       </div>
-      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%]  mt-5 text-[0.8rem] ">
+      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
         <ContactsContainer />
       </div>
     </>
