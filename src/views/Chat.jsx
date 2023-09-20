0eac062fca2c84bacfef3d9879b8fe661c0e3034
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-20 20:30:36 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -8,8 +8,9 @@ import SendMessage from "../components/ChatContainers/SendMessage";
 import Spinner from "../components/spinner";
 
 export default function Chat() {
-  const anchor = useRef()
+  const anchor = useRef();
 
+  const [activeChannel, setActiveChannel] = useState("");
   const [loadSpinner, setLoadSpinner] = useState(true);
   const [message, setMessage] = useState("");
   const [messageArray, setMessageArray] = useState([]);
@@ -21,28 +22,24 @@ export default function Chat() {
 
   const { user, logout, loading, searchOrCreateDocument, searchOrCreateImage } =
     useAuth();
-  useEffect( ( )  => {
+  useEffect(() => {
     getImage();
-    getDatos()
-    
+    getDatos();
   }, []);
 
-
-  async function SetMessageArrayFunction() {
-
-  }
+  async function SetMessageArrayFunction() {}
 
   async function getDatos() {
     const datosSearched = await searchOrCreateDocument(user.email);
     console.log(datosSearched);
-    setDatos(datosSearched)
+    setDatos(datosSearched);
     setLoadSpinner(false);
   }
   async function getImage() {
     setImage(await searchOrCreateImage(user.email, user.photoURL));
   }
 
-  console.log(messageArray)
+  console.log(messageArray);
   return (
     <div className="flex justify-center items-center h-screen relative">
       {loadSpinner ? (
@@ -50,18 +47,35 @@ export default function Chat() {
       ) : (
         <>
           <div className="bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
-            <Channels />
+            <Channels  setActiveChannel={setActiveChannel} />
           </div>
-          <div className="bg-[#343541] flex flex-col justify-center items-center h-full w-[80%]">
+          <div className="bg-[#343541] flex flex-col justify-start items-center h-full w-[80%]">
             <div className="border-b-[1px] border-[#646464] flex flex-col justify-center items-center h-[8%] w-full">
-              <UpBar />
+              <UpBar activeChannel={activeChannel}/>
             </div>
-            <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-start items-start h-[82%] w-full mt-2 overflow-y-scroll scrollbar"  >
-              <BodyMessage messageArray={messageArray} anchor={anchor}/>
-            </div>
-            <div className=" bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
-              <SendMessage setMessage={setMessage} message={message} messageArray={messageArray} setMessageArray={setMessageArray}  datos={datos} user={user.email} image={image} anchor={anchor}/>
-            </div>
+            {activeChannel ? (
+              <>
+                <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-start items-start h-[82%] w-full mt-2 overflow-y-scroll scrollbar">
+                  <BodyMessage messageArray={messageArray} anchor={anchor} />
+                </div>
+                <div className=" bg-[#343541] flex flex-col justify-center items-center h-[10%] w-full">
+                  <SendMessage
+                    setMessage={setMessage}
+                    message={message}
+                    messageArray={messageArray}
+                    setMessageArray={setMessageArray}
+                    datos={datos}
+                    user={user.email}
+                    image={image}
+                    anchor={anchor}
+                  />
+                </div>
+              </>
+            ) : (
+              <div className="border-b-[1px] border-[#646464] bg-[#343541] flex flex-col justify-center items-center h-[82%] w-full mt-2 overflow-y-scroll scrollbar">
+                <p>Elige un canal/contacto</p>
+              </div>
+            )}
           </div>
         </>
       )}
