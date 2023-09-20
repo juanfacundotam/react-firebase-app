0f12cb05a7662b00460da09d1d2bf523e3542b5a
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-20 19:45:09 -0300

    Update BodyMessage.jsx

--- src/components/ChatContainers/BodyMessage.jsx
+++ src/components/ChatContainers/BodyMessage.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import Message from "./BodyComponents/Message";
 import Perfil from "./BodyComponents/Perfil";
 
-export default function BodyMessage({ datos, image, messageArray }) {
+export default function BodyMessage({ datos, image, messageArray, anchor }) {
+
   const [currentName, setCurrentName] = useState("");
   return (
-    <div className=" flex flex-col justify-start items-start border-2 w-full h-fit">
+    <div className=" flex flex-col justify-start items-start border-2 w-full h-fit" ref={anchor}>
       {/* <Perfil datos={datos} image={image}/> */}
       {messageArray.map((item, index) => {
         return (
-          <div key={index} className="border-2 flex justify-center items-start w-full h-full text-white">
+          <div key={index}  className="border-2 flex justify-center items-start w-full h-full text-white">
         
               <img
-                src={image}
+                src={item.image}
                 className="mr-2  rounded-full w-10 h-10"
                 alt=""
               />
@@ -25,18 +26,18 @@ export default function BodyMessage({ datos, image, messageArray }) {
 
                
                   <h1 className="text-xs text-gray-200 mt-auto">
-                    {datos.nickName}
+                    {item.nickName}
                   </h1>
                   <h3 className="text-[0.6rem] text-gray-400 mt-auto">
-                    19/10/2023 19:30
+                    {item.date}
                   </h3>
               
-                <h2 className="text-[0.6rem] text-gray-400 mt-auto">{`"${datos.estado}"`}</h2>
+                <h2 className="text-[0.6rem] text-gray-400 mt-auto">{`"${item.estado}"`}</h2>
               </div>
 
 
               <div className="pl-2 text-[0.8rem] flex flex-col justify-start items-start w-full h-full gap-y-1 text-white ">
-                <p>{item}</p>
+                <p>{item.message}</p>
               </div>
             </div>
           </div>
