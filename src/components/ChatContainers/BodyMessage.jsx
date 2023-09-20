05ceabf2854106b12e618c9daa8d6620d9f1cd29
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-20 18:29:10 -0300

    Update BodyMessage.jsx

--- src/components/ChatContainers/BodyMessage.jsx
+++ src/components/ChatContainers/BodyMessage.jsx
@@ -5,34 +5,38 @@ import Perfil from "./BodyComponents/Perfil";
 export default function BodyMessage({ datos, image, messageArray }) {
   const [currentName, setCurrentName] = useState("");
   return (
-    <div className=" p-3 flex flex-col justify-center items-center w-full h-full overflow-y-auto overflow-x-hidden scrollbar">
+    <div className=" flex flex-col justify-start items-start border-2 w-full h-fit">
       {/* <Perfil datos={datos} image={image}/> */}
       {messageArray.map((item, index) => {
-
-
         return (
-          <div className="flex justify-center items-center w-full h-full">
-            <div className="flex justify-start items-start w-fit h-full">
+          <div className="border-2 flex justify-center items-start w-full h-full text-white">
+        
               <img
                 src={image}
                 className="mr-2  rounded-full w-10 h-10"
                 alt=""
               />
-            </div>
+
             <div className="flex flex-col justify-center items-end w-full h-full">
-              <div className="flex  justify-start items-end w-full h-[5%] gap-3">
-                <div className="flex gap-2">
+
+
+              <div className="flex  justify-start items-center w-full  gap-3">
+
+
+               
                   <h1 className="text-xs text-gray-200 mt-auto">
                     {datos.nickName}
                   </h1>
                   <h3 className="text-[0.6rem] text-gray-400 mt-auto">
                     19/10/2023 19:30
                   </h3>
-                </div>
+              
                 <h2 className="text-[0.6rem] text-gray-400 mt-auto">{`"${datos.estado}"`}</h2>
               </div>
-              <div className="pl-2 text-[0.8rem] flex flex-col justify-start items-start w-full h-full gap-y-1 ">
-                <p>{item.mensaje}</p>
+
+
+              <div className="pl-2 text-[0.8rem] flex flex-col justify-start items-start w-full h-full gap-y-1 text-white ">
+                <p>{item.mensaje}sssssssssss</p>
               </div>
             </div>
           </div>
