6c104cb6a4e06b4cfee00d77d8778e7111bfa416
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-22 13:15:17 -0300

    Update Channels.jsx

--- src/components/ChatContainers/Channels.jsx
+++ src/components/ChatContainers/Channels.jsx
@@ -4,20 +4,22 @@ import ContactsContainer from "./ChannelsComponents/ContactsContainer";
 import PublicChannels from "./ChannelsComponents/PublicChannels";
 import PrivateChannels from "./ChannelsComponents/PrivateChannels";
 
-export default function Channels({setActiveChannel}) {
+
+export default function Channels({setActiveChannel, channelsFiltered, contactFiltered}) {
+
 
 
   return (
     <>
     
      <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%] mt-5 text-[0.8rem] ">
-        <PublicChannels setActiveChannel={setActiveChannel}/>
+        <PublicChannels setActiveChannel={setActiveChannel} channel={channelsFiltered}/>
       </div>
       {/* <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[29%] mt-5 text-[0.8rem] ">
         <PrivateChannels />
       </div> */}
       <div className="flex flex-col justify-start items-start w-[80%] h-fit max-h-[45%] mt-5 text-[0.8rem] ">
-        <ContactsContainer setActiveChannel={setActiveChannel}/>
+        <ContactsContainer setActiveChannel={setActiveChannel} contact={contactFiltered}/>
       </div>
     </>
   );
