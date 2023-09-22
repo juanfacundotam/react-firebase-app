70a5cc50d9fddf7c0ce31725ca81282bcc8f98dd
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-22 13:15:26 -0300

    Update Chat.jsx

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -7,25 +7,42 @@ import BodyMessage from "../components/ChatContainers/BodyMessage";
 import SendMessage from "../components/ChatContainers/SendMessage";
 import Spinner from "../components/spinner";
 
+const messageData = [{name: "General", category: "channel",  message: []}, {name: "Ciclismo", category: "channel", message: []}, , {name: "Juan Pedro", category: "contact", message: []}]
+
+
 export default function Chat() {
+
+  const channelsFiltered = messageData.filter(channel => channel.category === "channel")
+  const contactFiltered = messageData.filter(channel => channel.category === "contact")
+
+  console.log(channelsFiltered)
+  console.log(contactFiltered)
+
   const anchor = useRef();
-
-  const [activeChannel, setActiveChannel] = useState("");
-  const [loadSpinner, setLoadSpinner] = useState(true);
-  const [message, setMessage] = useState("");
-  const [messageArray, setMessageArray] = useState([]);
-  const [datos, setDatos] = useState({
-    nickName: "NickName",
-    estado: "Estado",
-  });
-  const [image, setImage] = useState("");
-
   const { user, logout, loading, searchOrCreateDocument, searchOrCreateImage } =
     useAuth();
   useEffect(() => {
     getImage();
     getDatos();
   }, []);
+
+  const [activeChannel, setActiveChannel] = useState("");
+  const [loadSpinner, setLoadSpinner] = useState(true);
+  const [message, setMessage] = useState("");
+  const [messageArray, setMessageArray] = useState([]);
+  const [channelMessageArray, setChannelMessageArray] = useState([]);
+  const [contactMessageArray, setContactMessageArray] = useState([]);
+  const [image, setImage] = useState("");
+  const [datos, setDatos] = useState({
+    user: user.email,
+    nickName: "NickName",
+    estado: "Estado",
+    image: image,
+  });
+
+
+console.log(activeChannel)
+
 
   async function SetMessageArrayFunction() {}
 
@@ -47,7 +64,7 @@ export default function Chat() {
       ) : (
         <>
           <div className="bg-[#202123] flex flex-col justify-start items-center h-full w-[20%]">
-            <Channels  setActiveChannel={setActiveChannel} />
+            <Channels  setActiveChannel={setActiveChannel} channelsFiltered={channelsFiltered} contactFiltered={contactFiltered}/>
           </div>
           <div className="bg-[#343541] flex flex-col justify-start items-center h-full w-[80%]">
             <div className="border-b-[1px] border-[#646464] flex flex-col justify-center items-center h-[8%] w-full">
