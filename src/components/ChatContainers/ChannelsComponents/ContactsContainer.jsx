fcd4acf8e87145e15def35d393f8d6d4d6218cc6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-28 21:40:22 -0300

    Update ContactsContainer.jsx

--- src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
+++ src/components/ChatContainers/ChannelsComponents/ContactsContainer.jsx
@@ -1,33 +1,41 @@
 import React, {useState} from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import AddContactModal from "./AddContactModal"
 
 
 
-export default function ContactsContainer({setActiveChannel, contact, messageChats}) {
+export default function ContactsContainer({setActiveChannel, contact, messageChats, setNewContactFunction, searchAndLinkContact, setChannelName}) {
   const [openChannels, setOpenChannels] = useState(true)
+  
 
-  const setContact = (contact) => {
+  const setContact = (contact, nickName) => {
     setActiveChannel(contact)
+    setChannelName(nickName)
   }
 
 
   return (
 <>
-      <div className="flex justify-center items-center cursor-pointer hover:text-gray-300" 
-      onClick={() => {setOpenChannels(!openChannels)}}
+      <div className="flex justify-between items-center cursor-pointer w-full " 
       >
+        <div className="flex hover:text-gray-300" onClick={() => {setOpenChannels(!openChannels)}}>
         {openChannels ? (
-          <KeyboardArrowDownIcon sx={{ fontSize: 15, cursor: "pointer" }} />
+          <KeyboardArrowDownIcon sx={{ fontSize: 10, cursor: "pointer" }} />
         ) : (
-          <KeyboardArrowUpIcon sx={{ fontSize: 15, cursor: "pointer" }} />
+          <KeyboardArrowUpIcon sx={{ fontSize: 10, cursor: "pointer" }} />
         )}
         <h1>Contactos</h1>
+
+        </div>
+        <AddContactModal setNewContactFunction={setNewContactFunction} searchAndLinkContact={searchAndLinkContact}/>
+
       </div>
 
       {openChannels && <div className="mt-2 overflow-y-auto overflow-x-hidden scrollbar w-[100%] h-fit">
       {messageChats.map((item, index) => {
-        return (<p key={index} className="ml-5 cursor-pointer hover:text-gray-300" onClick={() => {setContact(item.id)}}># {item.id}</p>)
+        return (<p key={index} className="ml-5 cursor-pointer hover:text-gray-300" onClick={() => {setContact(item.id, item.data.nickName)}}># {item.data.nickName}</p>)
       })}
  
       </div>}
