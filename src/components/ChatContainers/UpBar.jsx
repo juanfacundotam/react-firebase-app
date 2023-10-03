525b6d34ce3bae2eee01200dc441b5fa8be36a61
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-03 18:35:45 -0300

    Update UpBar.jsx

--- src/components/ChatContainers/UpBar.jsx
+++ src/components/ChatContainers/UpBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate } from "react-router-dom";
+
 export default function UpBar({channelName}) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between items-center w-[98%] h-full">
       <div>
@@ -18,6 +23,17 @@ export default function UpBar({channelName}) {
           />
                 <SearchIcon sx={{ fontSize: 25, marginRight:1, cursor: "pointer", marginLeft: 1}}/>
         </div>
+                <HomeIcon         sx={{
+          fontSize: 25,
+          cursor: "pointer",
+          marginLeft: 3,
+          marginRight: 1,
+          transition: "color 0.3s ease",
+          "&:hover": {
+            color: "grey",
+            scale: "1.2",
+          },
+        }} onClick={() => navigate("/")}/>
 
 
 
