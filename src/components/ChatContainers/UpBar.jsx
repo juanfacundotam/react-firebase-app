2c3285487260c5c7de1a5555bda6a28880afa5fd
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-09 20:44:04 -0300

    Update UpBar.jsx

--- src/components/ChatContainers/UpBar.jsx
+++ src/components/ChatContainers/UpBar.jsx
@@ -3,13 +3,26 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function UpBar({channelName}) {
+export default function UpBar({iconsHidden, channelName, setMenuOpen}) {
   const navigate = useNavigate();
 
   return (
     <div className="flex justify-between items-center w-[98%] h-full">
-      <div>
+      <div  className="flex justify-between items-center gap-2">
+     { !iconsHidden &&<MenuIcon sx={{
+          fontSize: 25,
+          cursor: "pointer",
+          zIndex: 0,
+          marginLeft: 1,
+          marginRight: 1,
+          transition: "color 0.3s ease",
+          "&:hover": {
+            color: "grey",
+            scale: "1.1",
+          },
+        }} onClick={() => setMenuOpen(true)}/>}
         <h1># {channelName || "???"}</h1>
       </div>
       <div className="flex justify-between items-center gap-1 ">
